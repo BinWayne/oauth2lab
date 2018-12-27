d633319e428e8173c367a936ebbd4fcf4fc5afb0
binwayne <36578067@qq.com>
2018-12-27 15:18:42 +0800

    rollback to master base

--- lab01/authcode-server/src/main/java/io/spring2go/authcodeserver/api/UserController.java
+++ lab01/authcode-server/src/main/java/io/spring2go/authcodeserver/api/UserController.java
@@ -1,39 +1,14 @@
 package io.spring2go.authcodeserver.api;
 
-import java.sql.SQLException;
-
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.alibaba.druid.pool.DruidDataSource;
 
 @Controller
 public class UserController {
 
-	  @Autowired
-	    private DataSource dataSource;
-
-	    @RequestMapping("/index")
-	    @ResponseBody
-	    public String index() throws SQLException {
-	        System.out.println(dataSource.getConnection());
-	        if(dataSource instanceof DruidDataSource) {
-	        	System.out.println("true");
-	        	DruidDataSource dd = (DruidDataSource)dataSource;
-	        	dd.getConnection();
-	        	 System.out.println(dd);
-	        }
-	       
-	        return "hello spring boot";
-	    }
-	    
 	// 资源API
     @RequestMapping("/api/userinfo")
     public ResponseEntity<UserInfo> getUserInfo() {
